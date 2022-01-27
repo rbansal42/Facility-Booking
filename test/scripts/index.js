adaaed7a80de7cf7b008391e24728408cdea194e
Rahul Bansal <42.rahulbansal@gmail,com>
2022-01-28 01:48:22 +0530

    Removed garbage functions and begun writing function for submit

--- test/scripts/index.js
+++ test/scripts/index.js
@@ -1,6 +1,11 @@
 
 function input_click() {
-    document.getElementById("search_input").placeholder='';
-    document.getElementById("search_input").autocomplete="on";
+    document.getElementById("search_input").placeholder='Type to complete';
+    // document.getElementById("search_input").autocomplete="on";
+}
 
+function submit() {
+
+    var value = document.getElementById("search_input").value;
+    
 }
