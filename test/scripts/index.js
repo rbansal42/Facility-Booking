a209311e8e85930628bedf2e45d94f2d7722dd5c
Rahul Bansal <42.rahulbansal@gmail,com>
2022-01-28 02:06:08 +0530

    cleanup

--- test/scripts/index.js
+++ test/scripts/index.js
@@ -1,11 +1,10 @@
 
-function input_click() {
-    document.getElementById("search_input").placeholder='Type to complete';
-    // document.getElementById("search_input").autocomplete="on";
-}
-
 function submit() {
 
     var value = document.getElementById("search_input").value;
     
+    if(value == "Auditorium") {
+        window.location.href = "./";
+    }
+
 }
