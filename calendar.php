d2cdff7ef4ab13f46f139535830b10991aca2659
Rahul Bansal <42.rahulbansal@gmail,com>
2022-01-28 09:59:26 +0530

    Latest Build

---
+++ calendar.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Calender</title>
+        <link rel="stylesheet" href="./styles/dycalendar.css">
+        <link rel="stylesheet" href="./styles/styles.css">
+        
+    </head>
+    <body>
+
+    </body>
+    <header>
+        <div class="header">
+    
+          <div class="header_logo">
+            
+            <img
+            src="./sources/SU_logo_mono.svg" alt="Sushant University Logo"
+            height="60px"
+            width="auto">
+    
+          </div>
+    
+          <div class="header_title">
+           
+            Facilities Booking Portal
+            
+          </div>
+    
+          <div class="header_profile">
+    
+            <img src="./sources/profile.svg" alt="Profile"
+            height="20px">
+         
+          </div>
+    
+        </div>
+    
+      </header>
+    <section>
+        <div class="box">
+            <h1 id="facility_name">
+                
+                
+            </h1>
+        <div class="container">
+        <div id="dycalendar">
+
+        </div>
+        </div>
+
+        </div>
+    </section>
+    <script src="./scripts/dycalendar.js"></script>
+    <script>
+        dycalendar.draw({
+            target : '#dycalendar',
+            type: 'month',
+            dayformat: 'full',
+            monthformat: 'full',
+            highlighttargetdate: true,
+            prevnextbutton: 'show'
+
+        })
+
+    </script>
+</html>
